e2cff8e4e743e2d734437e93c23bc3c987489416
Patrick Ametepe <patrickamtepe@gmail.com>
2020-07-31 15:27:14 +0000

    login

--- app/Controllers/Users.php
+++ app/Controllers/Users.php
@@ -7,8 +7,36 @@ class Users extends BaseController
 {
 	public function index()
 	{
+        
         $data = [];
         helper(['form']);
+
+        if($this->request->getMethod() == 'post'){
+            // validation
+            $rules = [
+                'email' => 'required|min_length[6]|max_length[60]|valid_email|is_unique[users.email]',
+                'password' => 'required|min_length[6]|max_length[255]|validateUser[email,password]',
+            ];
+
+            $errors = [
+                'password' => [
+                    'validateUsers' => " Email or Password Don\'t match"
+                ]
+                ];
+
+            if (! $this->validate($rules)) {
+                $data['validation'] = $this->validator;
+            }else{
+                $model = new UserModel();
+                $newData = [
+                    'email' => $this->request->getVar('email'),
+                    'password' => $this->request->getVar('password'),
+                ];
+                $model->save($newData);$session = session();
+                $session->setFlashData('success','Successful Login');
+                return redirect()->to('/fishfarm_ci/public/');
+            }
+        }
 
         echo view('templates/header', $data);
         echo view('login', $data);
