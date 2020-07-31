4dbee90204cb6a80bc87bbe7f633c956f293019c
Patrick Ametepe <patrickamtepe@gmail.com>
2020-07-31 16:10:13 +0000

    dashboard controller created

---
+++ app/Controllers/Dashboard.php
@@ -0,0 +1,17 @@
+<?php namespace App\Controllers;
+
+class Dashboard extends BaseController
+{
+	public function index()
+	{
+        $data = [];
+        
+        
+        echo view('templates/header', $data);
+        echo view('dashboard', $data);
+        echo view('templates/footer', $data);
+	}
+
+	//--------------------------------------------------------------------
+
+}
