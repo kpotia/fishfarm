0dc8624468972debe898a17afd7489c86e87fa81
Patrick Ametepe <patrickamtepe@gmail.com>
2020-08-04 20:50:14 +0000

    dashboard card added

--- app/Views/index.php
+++ app/Views/index.php
@@ -1,11 +1,151 @@
 <?= $this->extend('templates/dashboard') ?>
 
 <?= $this->section('content') ?>
-<pre>
+    <div class="row">
+        <div class="col-lg-3 col-6">
+            <!-- small card -->
+            <div class="small-box bg-primary">
+                <div class="inner">
+                <h3>44</h3>
 
-<?php
-// var_dump($user);
-?>
-    
-</pre>
+                <p>Fish</p>
+                </div>
+                <div class="icon">
+                <i class="fas fa-fish"></i>
+                </div>
+                <a href="#" class="small-box-footer">
+                More info <i class="fas fa-arrow-circle-right"></i>
+                </a>
+            </div>
+        </div>
+        
+        <div class="col-lg-3 col-6">
+            <!-- small card -->
+            <div class="small-box bg-primary">
+                <div class="inner">
+                <h3>44</h3>
+
+                <p>Product</p>
+                </div>
+                <div class="icon">
+                <i class="fab fa-product-hunt"></i>
+                </div>
+                <a href="#" class="small-box-footer">
+                More info <i class="fas fa-arrow-circle-right"></i>
+                </a>
+            </div>
+        </div>
+        <div class="col-lg-2 col-6">
+            <!-- small card -->
+            <div class="small-box bg-primary">
+                <div class="inner">
+                <h3>44</h3>
+                <p>Staff</p>
+                </div>
+                <div class="icon">
+                <i class="fas fa-users"></i>
+                </div>
+                <a href="#" class="small-box-footer">
+                More info <i class="fas fa-arrow-circle-right"></i>
+                </a>
+            </div>
+        </div>
+        <div class="col-lg-2 col-6">
+            <!-- small card -->
+            <div class="small-box bg-primary">
+                <div class="inner">
+                <h3>44</h3>
+                <p>Food History</p>
+                </div>
+                <div class="icon">
+                <i class="fas fa-users"></i>
+                </div>
+                <a href="#" class="small-box-footer">
+                More info <i class="fas fa-arrow-circle-right"></i>
+                </a>
+            </div>
+        </div>
+        <div class="col-lg-2 col-6">
+            <!-- small card -->
+            <div class="small-box bg-primary">
+                <div class="inner">
+                <h3>44</h3>
+                <p>Vaccines</p>
+                </div>
+                <div class="icon">
+                <i class="fas fa-syringe"></i>
+                </div>
+                <a href="#" class="small-box-footer">
+                More info <i class="fas fa-arrow-circle-right"></i>
+                </a>
+            </div>
+        </div>
+
+        <div class="col-lg-3 col-6">
+            <!-- small card -->
+            <div class="small-box bg-primary">
+                <div class="inner">
+                <h3>5</h3>
+                <p>Suppliers</p>
+                </div>
+                <div class="icon">
+                <i class="fas fa-users"></i>
+                </div>
+                <a href="#" class="small-box-footer">
+                More info <i class="fas fa-arrow-circle-right"></i>
+                </a>
+            </div>
+        </div>
+
+        
+        
+        <div class="col-lg-3 col-6">
+            <!-- small card -->
+            <div class="small-box bg-warning">
+                <div class="inner">
+                <h3>44</h3>
+
+                <p>Expenses</p>
+                </div>
+                <div class="icon">
+                <i class="fas fa-money-bill-alt"></i>
+                </div>
+                <a href="#" class="small-box-footer">
+                More info <i class="fas fa-arrow-circle-right"></i>
+                </a>
+            </div>
+        </div>
+        <div class="col-lg-3 col-6">
+            <!-- small card -->
+            <div class="small-box bg-warning">
+                <div class="inner">
+                <h3>44</h3>
+
+                <p>Sales</p>
+                </div>
+                <div class="icon">
+                <i class="fas fa-money-bill-wave"></i>
+                </div>
+                <a href="#" class="small-box-footer">
+                More info <i class="fas fa-arrow-circle-right"></i>
+                </a>
+            </div>
+        </div>
+        <div class="col-lg-3 col-6">
+            <!-- small card -->
+            <div class="small-box bg-warning">
+                <div class="inner">
+                <h3>44</h3>
+
+                <p>Purchase</p>
+                </div>
+                <div class="icon">
+                <i class="fas fa-receipt"></i>
+                </div>
+                <a href="#" class="small-box-footer">
+                More info <i class="fas fa-arrow-circle-right"></i>
+                </a>
+            </div>
+        </div>
+    </div>
 <?= $this->endSection() ?>
