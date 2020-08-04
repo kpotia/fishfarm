4e91ae5a7b8cd2d505a66723a53421bd9bb147f1
Patrick Ametepe <patrickamtepe@gmail.com>
2020-08-04 23:11:11 +0000

    dasboard controller working

--- app/Controllers/Dashboard.php
+++ app/Controllers/Dashboard.php
@@ -4,14 +4,15 @@ class Dashboard extends BaseController
 {
 	public function index()
 	{
+		$session = session(); 
 		$user = [
-			'id'=> session()->get('id'),
-			'firstname'=> session()->get('firstname'),
-			'lastname'=> session()->get('lastname'),
-			'email'=> session()->get('email'),			
+			'id'=> $session->get('id'),
+			'firstname'=> $session->get('firstname'),
+			'lastname'=> $session->get('lastname'),
+			'email'=> $session->get('email'),			
 		];
 
-		if(session()->isLoggedIn){
+		if($session->isLoggedIn){
 			$data = [
 				'title' => 'FishFarm Dashboard',
 				'user' => $user
