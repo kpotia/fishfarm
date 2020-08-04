ea273abc9442b684049a8cde9ddd9f62b38ac5bd
Patrick Ametepe <patrickamtepe@gmail.com>
2020-08-04 19:29:12 +0000

    dashboard template updated

--- app/Views/templates/dashboard.php
+++ app/Views/templates/dashboard.php
@@ -17,13 +17,16 @@
       <div class="container-fluid">
         <div class="row mb-2">
           <div class="col-sm-6">
-            <h1>FishFarm</h1>
+            <h1>
+            <?php if(isset($title)):  ?>
+              <?=$title ?>
+                <?php else:?>
+              Fishfarm
+            <?php endif;?>
+            </h1>
           </div>
           <div class="col-sm-6">
-            <!-- <ol class="breadcrumb float-sm-right">
-              <li class="breadcrumb-item"><a href="#">Home</a></li>
-              <li class="breadcrumb-item active">Blank Page</li>
-            </ol> -->
+          
           </div>
         </div>
       </div><!-- /.container-fluid -->
