ff4649f1910029015c98c238364616e4a9bfd583
Patrick Ametepe <patrickamtepe@gmail.com>
2020-08-08 00:02:57 +0000

    update link of the button

--- app/Views/FishListing.php
+++ app/Views/FishListing.php
@@ -18,8 +18,8 @@
                         <td> <?=$fish['name'] ?></td>
                         <td> <?=$fish['description'] ?></td>
                         <td>
-                            <a href="edit/" class="btn">edit</a> 
-                            <a href="delete/" class="btn">delete</a> 
+                            <a href="fish/edit/<?=$fish['id'] ?>" class="btn">edit</a> 
+                            <a href="fish/delete/<?=$fish['id'] ?>" class="btn">delete</a> 
                         </td>
                     </tr>
                 <?php endforeach; ?>
