b73fe2395cf82123258c03bac1c6a9a46290f26c
Patrick Ametepe <patrickamtepe@gmail.com>
2020-08-11 07:37:08 +0000

    fix register

--- app/Controllers/Users.php
+++ app/Controllers/Users.php
@@ -88,9 +88,30 @@ class Users extends BaseController
 
         if($this->request->getMethod() == 'post'){
             // validation
-            $this->registration();
+            $rules = [
+                'firstname' => 'required|min_length[3]|max_length[30]',
+                'lastname' => 'required|min_length[3]|max_length[30]',
+                'email' => 'required|min_length[6]|max_length[60]|valid_email|is_unique[users.email]',
+                'password' => 'required|min_length[6]|max_length[255]',
+                'confirm_password' => 'matches[password]',
+            ];
+    
+            if (! $this->validate($rules)) {
+                $data['validation'] = $this->validator;
+            }else{
+                $model = new UserModel();
+                $newData = [
+                    'firstname' => $this->request->getVar('firstname'),
+                    'lastname' => $this->request->getVar('lastname'),
+                    'email' => $this->request->getVar('email'),
+                    'password' => $this->request->getVar('password'),
+                ];
+                $model->save($newData);$session = session();
+                $session->setFlashData('success','Successful Registration');
+                return redirect()->to('/fishfarm_ci/public/');
+            }
         }
-
+        
         echo view('templates/header', $data);
         echo view('register', $data);
     }
