368de116809a398fc9cb8f1ca065f2d3ef1ea5a1
Patrick Ametepe <patrickamtepe@gmail.com>
2020-08-24 00:37:21 +0000

    add setting and backup methods

---
+++ app/Controllers/Setting.php
@@ -0,0 +1,33 @@
+<?php namespace App\Controllers;
+
+
+class Setting extends BaseController
+{
+	public function index()
+    {
+        // load settings view
+    }
+
+    // list backup
+    public function lustbackup(){
+        // load helper n libraries 
+
+        // go to folder 
+
+        // fetch backups 
+
+        // transfer backups to views 
+        
+    }
+
+    public function backup(){
+        // load database utility 
+
+        // backup database 
+
+        // save the backup 
+
+        // download saved files 
+    }
+        
+}
