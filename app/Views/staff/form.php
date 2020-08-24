e915304819262fb72c904494528962c81d549ec6
Patrick Ametepe <patrickamtepe@gmail.com>
2020-08-24 17:40:32 +0000

    staff form done

--- app/Views/staff/form.php
+++ app/Views/staff/form.php
@@ -4,14 +4,35 @@
 <div class="row">
     <div class="col-md-6">
         <form action="" method="post">
-            <div class="form-group">
-                <label for="Staff Name">Staff Name</label>
-                <input class="form-control" type="text" value="<?= $fish['name'] ?? set_value('Staff Name')?>"  required name="Staff Name">
+        <div class="form-group">
+                <label>Staff Firstname</label>
+                <input class="form-control" type="text" value="<?= $fish['firstname'] ?? set_value('firstname')?>"  required name="firstame">
             </div>
             <div class="form-group">
-                <label for="my-textarea">Fish Description</label>
-                <textarea id="my-textarea" class="form-control" required name="fishDescription" rows="3"><?= $fish['description'] ?? set_value('fishDescription')?></textarea>
+                <label>Staff Surname</label>
+                <input class="form-control" type="text" value="<?= $fish['surname'] ?? set_value('surname')?>"  required name="surname">
             </div>
+
+            <div class="form-group">
+                <label>Staff Email</label>
+                <input class="form-control" type="text" value="<?= $fish['email'] ?? set_value('email')?>"  required name="email">
+            </div>
+
+            <div class="form-group">
+                <label>Staff Contact</label>
+                <input class="form-control" type="text" value="<?= $fish['contact'] ?? set_value('contact')?>"  required name="contact">
+            </div>
+
+            <div class="form-group">
+                <label>Staff Address</label>
+                <input class="form-control" type="text" value="<?= $fish['address'] ?? set_value('address')?>"  required name="address">
+            </div>
+
+            <div class="form-group">
+                <label>Staff role</label>
+                <input class="form-control" type="text" value="<?= $fish['role'] ?? set_value('role')?>"  required name="role">
+            </div>
+            
             <?php if(isset($validation)):?>
           <div class="col-12">
             <div class="alert alert-danger" role="alert">
@@ -23,8 +44,6 @@
             <div class="btn-group" role="group" aria-label="Button group">
                 <button name="submit" value="submit" >Submit</button>
             </div>
-
-
         </form>
     </div>   
 </div>
