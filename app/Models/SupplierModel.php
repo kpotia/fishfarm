39c1e95d274dbd78a9a9757b988ded70a79f0858
Patrick Ametepe <patrickamtepe@gmail.com>
2020-08-24 11:38:22 +0000

    add Supplier Model

---
+++ app/Models/SupplierModel.php
@@ -0,0 +1,23 @@
+<?php namespace App\Models;
+
+use CodeIgniter\Model;
+
+class SupplierModel extends Model
+{
+    protected $table      = 'supplier';
+    protected $primaryKey = 'id';
+
+    protected $returnType     = 'array';
+    protected $useSoftDeletes = true;
+
+    protected $allowedFields = ['name', 'description','contact','email','address'];
+
+    protected $useTimestamps = false;
+    protected $createdField  = 'created_at';
+    protected $updatedField  = 'updated_at';
+    protected $deletedField  = 'deleted_at';
+
+    protected $validationRules    = [];
+    protected $validationMessages = [];
+    protected $skipValidation     = false;
+}
