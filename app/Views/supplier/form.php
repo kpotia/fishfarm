9f0d598938c3b7720cd5dc9e37252c80a772d32d
Patrick Ametepe <patrickamtepe@gmail.com>
2020-08-24 12:08:33 +0000

    build supplier form view

--- app/Views/supplier/form.php
+++ app/Views/supplier/form.php
@@ -4,22 +4,37 @@
 <div class="row">
     <div class="col-md-6">
         <form action="" method="post">
-            <div class="form-group">
-                <label for="FishName">Fish Name</label>
-                <input class="form-control" type="text" value="<?= $fish['name'] ?? set_value('fishName')?>"  required name="fishName">
+        <div class="form-group">
+                <label>Supplier Name</label>
+                <input class="form-control" type="text" value="<?= $supplier['name'] ?? set_value('name')?>"  required name="name">
             </div>
             <div class="form-group">
-                <label for="my-textarea">Fish Description</label>
-                <textarea id="my-textarea" class="form-control" required name="fishDescription" rows="3"><?= $fish['description'] ?? set_value('fishDescription')?></textarea>
+                <label>Supplier Contact</label>
+                <input class="form-control" type="text" value="<?= $supplier['contact'] ?? set_value('contact')?>"  required name="contact">
             </div>
-            <?php if(isset($validation)):?>
-          <div class="col-12">
-            <div class="alert alert-danger" role="alert">
-              <?= $validation->listErrors();?>
-            </div>
-          </div>
 
-        <?php endif;?>
+            <div class="form-group">
+                <label>Supplier Address</label>
+                <input class="form-control" type="text" value="<?= $supplier['address'] ?? set_value('address')?>"  required name="address">
+            </div>
+
+            <div class="form-group">
+                <label>Supplier Email</label>
+                <input class="form-control" type="text" value="<?= $supplier['email'] ?? set_value('email')?>"  required name="email">
+            </div>
+
+            <div class="form-group">
+                <label for="my-textarea">Supplier Description</label>
+                <textarea id="my-textarea" class="form-control" required name="Description" rows="3"><?= $supplier['description'] ?? set_value('description')?></textarea>
+            </div>
+            
+            <?php if(isset($validation)):?>
+                <div class="col-12">
+                    <div class="alert alert-danger" role="alert">
+                        <?= $validation->listErrors();?>
+                    </div>
+                </div>
+            <?php endif;?>
             <div class="btn-group" role="group" aria-label="Button group">
                 <button name="submit" value="submit" >Submit</button>
             </div>
