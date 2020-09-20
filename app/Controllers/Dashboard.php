044e2190fe958d0a5150b2f46b3f10e288acdf2a
Mr Kpoti <patrickamtepe@gmail.com>
2020-09-20 13:19:35 +0000

    Include DB and writting query

--- app/Controllers/Dashboard.php
+++ app/Controllers/Dashboard.php
@@ -6,6 +6,13 @@ class Dashboard extends BaseController
 	{
 		$session = session();
 
+		// load db and connect to db;
+		$db = \Config\Database::connect();
+		$query= 'SELECT COUNT(*) as fishcount from fish';
+		$query= 'SELECT COUNT(*) from fishtank';
+		$query= 'SELECT COUNT(*) from fish';
+
+
 		if($session->isLoggedIn){
 			$data = [
 				'title' => 'FishFarm Dashboard',
