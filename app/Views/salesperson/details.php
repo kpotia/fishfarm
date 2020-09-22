b383a2ac4b3c03d852092fb2f82e6ccadda83654
Mr Kpoti <patrickamtepe@gmail.com>
2020-09-22 12:37:05 +0000

    create sales person registration views

---
+++ app/Views/salesperson/details.php
@@ -0,0 +1,31 @@
+<?= $this->extend('templates/dashboard') ?>
+
+<?= $this->section('content') ?>
+<div class="row">
+
+    <div class="col-10 bg-white">
+
+        <div class="card">
+            <div class="card-header">
+                <h5 class="card-title">Client Details</h5>
+            </div>
+            <div class="card-body">
+                <ul class="list-unstyled">
+                    
+                    <li> ID: <?=$client['id']?></li>
+                    <li> Name: <?=$client['firstname'].' '.$client['surname']?></li>
+                    <li> Contact: <?=$client['contact']?></li>
+                    <li> Email: <?=$client['email']?></li>
+                    
+                    <li> Address: <?=$client['address']?></li>
+                    <a href="client/edit/<?=$client['id'] ?>" class="btn btn-secondary">edit</a>
+                    <a href="client/delete/<?=$client['id'] ?>" class="btn btn-danger">delete</a>
+                    
+                </ul>
+            </div>
+        </div>
+
+    </div>
+
+</div>
+<?= $this->endSection() ?>
---
+++ app/Views/salesperson/listing.php
@@ -0,0 +1,44 @@
+<?= $this->extend('templates/dashboard') ?>
+
+<?= $this->section('content') ?>
+<div class="row">
+
+    <div class="col-10 bg-white">
+        <?php if(session()->get('success')): ?>
+            <div class="alert alert-success" role="alert">
+                <?= session()->get('success') ?>
+            </div>
+        <?php elseif(session()->get('fail')): ?>
+            <?= session()->get('fail') ?>
+        <?php endif;?>
+        <table class='table table-responsive table-hover '>
+            <thead>
+                <tr>
+                    <th>client ID</th>
+                    <th>client Name</th>
+                    <th>client email</th>
+                    <th>client contact</th>
+                    <th>client address</th>
+                    <th>Options</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach($clients as $client): ?>
+                <tr>
+                    <td> <?=$client['id'] ?></td>
+                    <td> <?=$client['firstname'].' '.$client['surname'] ?></td>
+                    <td> <?=$client['email'] ?></td>
+                    <td> <?=$client['contact'] ?></td>
+                    <td> <?=$client['address'] ?></td>
+                    <td>
+                        <a href="client/edit/<?=$client['id'] ?>" class="btn btn-secondary">edit</a>
+                        <a href="client/delete/<?=$client['id'] ?>" class="btn btn-danger">delete</a>
+                    </td>
+                </tr>
+                <?php endforeach; ?>
+            </tbody>
+        </table>
+    </div>
+
+</div>
+<?= $this->endSection() ?>
