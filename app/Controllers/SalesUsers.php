20161877f58bf9442f72805206b7464ffdae67da
Mr Kpoti <patrickamtepe@gmail.com>
2020-09-22 12:43:04 +0000

    remove unused register private function

--- app/Controllers/SalesUsers.php
+++ app/Controllers/SalesUsers.php
@@ -138,33 +138,13 @@ class SalesUsers extends BaseController
         echo view('register', $data);
     }
 
-    private function registration(){
-        $rules = [
-            'firstname' => 'required|min_length[3]|max_length[30]',
-            'lastname' => 'required|min_length[3]|max_length[30]',
-            'email' => 'required|min_length[6]|max_length[60]|valid_email|is_unique[users.email]',
-            'password' => 'required|min_length[6]|max_length[255]',
-            'confirm_password' => 'matches[password]',
-        ];
-
-        if (! $this->validate($rules)) {
-            $data['validation'] = $this->validator;
-        }else{
-            $model = new UserModel();
-            $newData = [
-                'firstname' => $this->request->getVar('firstname'),
-                'lastname' => $this->request->getVar('lastname'),
-                'email' => $this->request->getVar('email'),
-                'password' => $this->request->getVar('password'),
-            ];
-            $model->save($newData);$session = session();
-            $session->setFlashData('success','Successful Registration');
-            return redirect()->to('/fishfarm_ci/public/');
-        }
-    }
-
     public function logout(){
         session()->destroy();
         return redirect()->to('/fishfarm_ci/public/');
+    }
+
+
+    public list(){
+
     }
 }
